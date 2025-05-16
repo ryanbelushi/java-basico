a58565b4c683d708125efa0dd002bf417185f469
Ryan Belushi <belushiryan2@gmail.com>
2025-05-16 11:26:02 -0300

    Operador ternario

---
+++ Operadores/Operadores.java
@@ -0,0 +1,10 @@
+package Operadores;
+
+public class Operadores {
+public static void main(String[] args) {
+    // Operador ternario
+    int a = 10;
+    int b = 20;
+    int maior = (a > b) ? a : b; // Se a for maior que b, maior recebe a, senão recebe b
+}
+}
